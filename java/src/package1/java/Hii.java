f62f95b727ac254aa3bd781019510df1bbb37a4c
mahimasharma4 <62322628+mahimasharma4@users.noreply.github.com>
2020-03-23 21:05:42 +0530

    Everything is fine

--- java/src/package1/java/Hii.java
+++ java/src/package1/java/Hii.java
@@ -3,6 +3,7 @@ package package1.java;
 public class Hii {
     public static void main (String args[] ){
         System.out.println("hii");
+        System.out.println("Everything is Fine");
 
     }
 }
