d1b0af11372a8c9246026de6d57f6bbe70ad2dff
mahimasharma4 <62322628+mahimasharma4@users.noreply.github.com>
2020-03-23 21:09:47 +0530

    ab me commit msg type kar raha hu
    yaha pr discription likhna ota hai.
    jo bade program me kaam aata hai.
    but tuje kane ki jarut nhi hai.

--- java/src/package1/java/Hii.java
+++ java/src/package1/java/Hii.java
@@ -4,6 +4,7 @@ public class Hii {
     public static void main (String args[] ){
         System.out.println("hii");
         System.out.println("Everything is Fine");
+        System.out.println("Dyan se dekhna");
 
     }
 }
